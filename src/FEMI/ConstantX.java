5c7f68533d23ad78808bc5193e6ef0936bf4f04c
James DivineMercy <iamoluchimercy@gmail.com>
2023-09-15 06:18:18 +0100

    Another update on chapter 4

---
+++ src/FEMI/ConstantX.java
@@ -0,0 +1,25 @@
+package FEMI;
+
+import java.util.Scanner;
+
+public class ConstantX {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of terms you want to calculate: ");
+        int terms = scan.nextInt();
+
+        System.out.println("Enter the base: ");
+        int base = scan.nextInt();
+
+        double factorial = 1;
+        double sum = 0;
+
+        for (int count = 1; count <= terms; count++){
+            factorial *= count;
+            double divisor = Math.pow(base,count) / factorial;
+            sum += divisor;
+
+        }
+        System.out.printf("The constant eX is %.6f",sum + 1);
+    }
+}
