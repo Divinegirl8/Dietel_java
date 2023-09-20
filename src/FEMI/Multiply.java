659e5872f789881333cc5ae6db6083b6016fbdf3
James DivineMercy <iamoluchimercy@gmail.com>
2023-09-20 23:25:48 +0100

    updated multiply

--- src/FEMI/Multiply.java
+++ src/FEMI/Multiply.java
@@ -1,5 +1,31 @@
 package FEMI;
 
+//public class Multiply {
+//
+//    public static int multiply(int firstNumber,int secondNUmber) {
+//
+//        int result = 0;
+//        int count;
+//
+//        if (secondNUmber <= 0) {
+//            for (count = firstNumber; count >= 1; count--) {
+//                result += secondNUmber;
+//            }
+//        }
+//
+//
+//        else {
+//            for (count = 1; count <= secondNUmber; count++) {
+//                result += firstNumber;
+//            }}
+//
+//            return result;
+//        }
+//
+//
+//
+//}
+
 public class Multiply {
 
     public static int multiply(int firstNumber,int secondNUmber) {
@@ -8,21 +34,38 @@ public class Multiply {
         int count;
 
         if (secondNUmber <= 0) {
-            for (count = firstNumber; count >= 1; count--) {
-                result += secondNUmber;
+          for (count = firstNumber; count >= 1; count--) {
+               result += secondNUmber;
+         }
+       }
+
+
+        if (firstNumber > secondNUmber) {
+            for (count = 0; count > firstNumber; count--) {
+                result -= secondNUmber;
             }
         }
 
-        else {
-            for (count = 1; count <= secondNUmber; count++) {
-                result += firstNumber;
-            }}
-
-            return result;
+        if (secondNUmber > firstNumber) {
+            for (count = 0; count > secondNUmber; count--) {
+                result -= firstNumber;
+            }
         }
+
+
+        for (count = 1; count <= secondNUmber; count++) {
+            result += firstNumber;
+            }
+
+
+
+
+        return result;
+    }
 
 
 
 }
+
 
 
--- test/FEMI/MultiplyTest.java
+++ test/FEMI/MultiplyTest.java
@@ -24,4 +24,65 @@ class MultiplyTest {
         assertEquals(6,times);
     }
 
+    @Test
+    public void testSecondNumberLess() {
+        int times = Multiply.multiply(-2 ,-4);
+        assertEquals(8,times);
+    }
+
+    @Test
+    public void testFirstNumberLess() {
+        int times = Multiply.multiply(-4, -2);
+        assertEquals(8,times);
+    }
+
+    @Test
+    public void testZeroinSecondNumber() {
+        int times = Multiply.multiply(-4, 0);
+        assertEquals(0,times);
+    }
+    
+    @Test
+    public void testZeroInFirstNumber() {
+        int times = Multiply.multiply(0, -4);
+        assertEquals(0,times);
+    }
+
+    @Test
+    public void testZeroinBothNumber() {
+        int times = Multiply.multiply(0, 0);
+        assertEquals(0,times);
+    }
+
+    @Test
+    public void testMinusZeroinBothNumber() {
+        int times = Multiply.multiply(-0, -0);
+        assertEquals(0,times);
+    }
+
+    @Test
+    public void testMinusAndPlus() {
+        int times = Multiply.multiply(+4, -2);
+        assertEquals(-8,times);
+    }
+
+    @Test
+    public void testMinusAndPlusTwo() {
+        int times = Multiply.multiply(-2, +4);
+        assertEquals(-8,times);
+    }
+
+    @Test
+    public void testZero() {
+        int times = Multiply.multiply(0, +4);
+        assertEquals(0,times);
+    }
+
+    @Test
+    public void testZeroTwo() {
+        int times = Multiply.multiply(+4, 0);
+        assertEquals(0,times);
+    }
+
+
 }
