61fe5e70e739b605e62a7f4885fcbe953113aa9c
James DivineMercy <iamoluchimercy@gmail.com>
2023-09-27 15:46:43 +0100

    Array Assignment

--- src/Array/ScoresInArray.java
+++ src/Array/ScoresInArray.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 
 public class ScoresInArray {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[] listArray = new int[10];
+        System.out.println(arrayLargeNumber(10));
 
-         int count;
-         int largest = 0;
-        for (count = 0; count < listArray.length; count++){
+    }
+
+    public static int arrayLargeNumber(int integer){
+        Scanner scan = new Scanner(System.in);
+
+        int[] scores = new int[integer];
+
+        int count;
+        int largest = 0;
+        for (count = 0; count < scores.length; count++){
             System.out.print("Enter score: ");
             int score = scan.nextInt();
-            listArray[count] = score;
+
+            scores[count]  = score;
         }
 
-       for (int number: listArray){
-           if (number > largest){
-               largest = number;
-           }
-       }
+        for (int number: scores){
+            if (number > largest){
+                largest = number;
+            }
+        }
 
-        System.out.print("The largest number is " + largest);
-
-
-
+        return largest;
     }
 }
